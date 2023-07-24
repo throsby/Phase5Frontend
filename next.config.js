114a9a40fb54d62a4f23e9428c4801d620b4ba58
Throsby <throsbywells@gmail.com>
2023-07-24 19:23:11 -0400

    removed end '/' from next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         // RUBY_BACKEND_URL: process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? (console.log("Dev environment"))() : "",
-        RUBY_BACKEND_URL: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://cool-lake-5286.fly.dev/" : "http://localhost:3000",
+        RUBY_BACKEND_URL: process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? "https://cool-lake-5286.fly.dev" : "http://localhost:3000",
     },
 };
